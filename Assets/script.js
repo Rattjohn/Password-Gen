67d36ebcec146a252e4cb4e4f5efdecb5d52acb5
ian rattigan <ian.rattigan2000@gmail.com>
2023-04-20 23:27:04 -0400

    finish the project by polishing the loops that cause my project to break

--- Assets/script.js
+++ Assets/script.js
@@ -8,14 +8,14 @@ var superString = ""
 
 
 function getRandomNumber(min, max) {
-  var randomNumber=Math.random() // Random number between 0 and 0.99
-  var randomNumberUpToMax=randomNumber * max // Random number between 0 and max - 0.1
-  var randomNumberInRange= min + randomNumberUpToMax;
-  return Math.floor(randomNumberInRange); // Remove the decimal places
+  var randomNumber = Math.random()
+  var randomNumberUpToMax = randomNumber * max
+  var randomNumberInRange = min + randomNumberUpToMax;
+  return Math.floor(randomNumberInRange);
 }
 
 function getRandomValueFromArray(array) {
-  var randomArrayPosition=getRandomNumber(0,array.length);
+  var randomArrayPosition = getRandomNumber(0, array.length);
   return array[randomArrayPosition];
 }
 
@@ -23,6 +23,10 @@ function getRandomValueFromArray(array) {
 function generatePassword() {
   var passwordLength = prompt("Choose password length between 8 to 128.")
   console.log("This is the password length!", passwordLength)
+  while(passwordLength<8 || passwordLength>128){
+    passwordLength = prompt("Invalid password length, please choose length between 8 to 128.")
+  }
+
   var lowerCase = confirm("include Lowercases?");
   console.log("lowercase: ", lowerCase)
   var upperCase = confirm("include Uppercases?");
@@ -31,61 +35,54 @@ function generatePassword() {
   console.log("numerics: ", numEric)
   var specialCharacters = confirm("include special characters?");
   console.log("special characters: ", specialCharacters)
-  
-  if (lowerCase) {
-    superString=superString+getRandomValueFromArray(lowercaseString) 
-      console.log(superString)
 
-};
+  if (!lowerCase && !upperCase && !numEric && !specialCharacters) {
+    alert("You must choose characters for the password!")
+    generatePassword()
+  };
 
-if (upperCase) {
-  superString=superString+getRandomValueFromArray(uppercaseString) 
-    console.log(superString)
+  superString = ""
 
-};
-
-
-if (numEric) {
-  superString=superString+getRandomValueFromArray(numericString) 
-    console.log(superString)
-
-};
-
-if (specialCharacters) {
-  superString=superString+getRandomValueFromArray(specialcharacterString) 
-    console.log(superString)
-
-};
-
-for(var i=0;i<passwordLength;i++) {
-  
+  while (superString.length < passwordLength) {
+    if (superString.length == passwordLength) {
+      break;
+    };
+    if (lowerCase) {
+      superString = superString + getRandomValueFromArray(lowercaseString)
+    };
+    if (superString.length == passwordLength) {
+      break;
+    };
+    if (upperCase) {
+      superString = superString + getRandomValueFromArray(uppercaseString)
+    };
+    if (superString.length == passwordLength) {
+      break;
+    };
+    if (numEric) {
+      superString = superString + getRandomValueFromArray(numericString)
+    };
+    if (superString.length == passwordLength) {
+      break;
+    };
+    if (specialCharacters) {
+      superString = superString + getRandomValueFromArray(specialcharacterString)
+      
+    };
+  }
+  return superString
 }
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
 
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+function writePassword(event) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
